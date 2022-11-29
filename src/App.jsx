c6fa4c1ba60017a89443c1b317de3701c0a92bf0
Mohit Raj Singh <mohit94e@gmail.com>
2022-11-29 16:55:24 +0530

    layout ready

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import About from "./components/about/About";
 import Experience from "./components/experience/Experience";
 import Services from "./components/services/Services";
 import Portfolio from "./components/portfolio/Portfolio";
-import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 
@@ -18,7 +17,6 @@ export const App = () => {
       <Experience />
       <Services />
       <Portfolio />
-      <Testimonials />
       <Contact />
       <Footer />
     </>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ import {IoLogoTwitter} from 'react-icons/io'
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className="footer__logo">Logo</a>
+      <a href="#" className="footer__logo">Home</a>
       <ul className="permalinks">
         <li><a href="#">Home</a></li>
         <li><a href="#about">About</a></li>
