2f3cd035269a11a252f1696fa0b4a777a651a5f5
Mohit Raj Singh <mohit94e@gmail.com>
2022-12-08 12:35:54 +0530

    another change

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -18,17 +18,17 @@ const About = () => {
             <article className="about__card">
               <FaAward className="about__icon" />
               <h5>Experience</h5>
-              <small>3+ Years Working</small>
+              <small>1+ Years Working</small>
             </article>
             <article className="about__card">
               <FiUsers className="about__icon" />
-              <h5>Clients</h5>
-              <small>200+ Worldwide</small>
+              <h5>Coding Practice</h5>
+              <small>1000+ Hours</small>
             </article>
             <article className="about__card">
               <VscFolderLibrary className="about__icon" />
               <h5>Projects</h5>
-              <small>80+ Completed</small>
+              <small>6 Completed</small>
             </article>
           </div>
           <p>
