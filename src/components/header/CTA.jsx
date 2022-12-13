a15c9bf153a670494fb3d8f6aa707551ee91e152
Mohit Raj Singh <mohit94e@gmail.com>
2022-12-13 11:22:57 +0530

    port

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -5,7 +5,7 @@ const CTA = () => {
   return (
     <div className="cta">
       <a href={Resume} download className="btn">
-        Download CV
+        Download Resume
       </a>
       <a href="#contact" className="btn btn-primary">Let's Talk</a>
     </div>
