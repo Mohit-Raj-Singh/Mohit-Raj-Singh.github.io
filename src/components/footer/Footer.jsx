2f0a68a40bdb931b8ea39827e4826e39787f0399
Mohit Raj Singh <mohit94e@gmail.com>
2022-12-24 23:35:14 +0530

    social change

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,14 +10,20 @@ const Footer = () => {
     <footer>
       <a href="#" className="footer__logo">Home</a>
       <ul className="permalinks">
-        <li><a href="https://www.linkedin.com/in/mohit-raj-singh-m6164/" target="_blank"><BsLinkedin className='logoLink'/></a></li>
         <li><a href="#">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#skills">Skills</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
         <li><a href="#contact">Contact</a></li>
-        <li><a href="https://github.com/Mohit-Raj-Singh" target="_blank"><FaGithub className='logoGit'/></a></li>
       </ul>
+
+      <div className="footer__socials">
+      <a href="https://www.linkedin.com/in/mohit-raj-singh-m6164/"><BsLinkedin/></a>
+      <a href="https://www.instagram.com/iam_mohitrajs/"><FiInstagram/></a>
+       <a href="https://github.com/Mohit-Raj-Singh"><FaGithub/></a>
+       
+       
+      </div>
     </footer>
   )
 }
