cf30ee03e3104b480e2c2c21bc65daf70428fd30
Mohit Raj Singh <mohit94e@gmail.com>
2023-01-02 22:17:34 +0530

    logo added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,11 +4,13 @@ import {FaFacebookF, FaGithub} from 'react-icons/fa'
 import {FiInstagram} from 'react-icons/fi'
 import {IoLogoTwitter} from 'react-icons/io'
 import { BsLinkedin } from 'react-icons/bs'
+import Logo from "../../assets/logo.png";
 
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className="footer__logo">Home</a>
+      <a href="#" className="footer__logo"><img src={Logo} alt='logo' /></a>
+      {/* <a href="#" className="footer__logo">Home</a> */}
       <ul className="permalinks">
         <li><a href="#">Home</a></li>
         <li><a href="#about">About</a></li>
