9733993604afebfeb24017c9d67289a540abd4d5
Mohit Raj Singh <mohit94e@gmail.com>
2023-01-21 22:58:19 +0530

    resume open in new tab

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,8 +4,9 @@ import Resume from "../../assets/Mohit-Raj-Singh-Resume.pdf";
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={Resume} download className="btn">
-        Download Resume
+      {/* <a href={Resume} download className="btn"> */}
+      <a href="https://drive.google.com/file/d/1pU66hMY_E8PabIlRXU-TdMBk_0yRGUEA/view?usp=sharing" target="_blank" download className="btn">
+        Resume
       </a>
       <a href="#contact" className="btn btn-primary">Let's Talk</a>
     </div>
