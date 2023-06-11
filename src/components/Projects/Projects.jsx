ead015aeceb296b0b65278eaa09e4939f1ab2c7b
Mohit Raj Singh <mohit94e@gmail.com>
2023-06-11 12:35:16 +0530

    github bug fixed

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -16,7 +16,7 @@ import booking from "../../Assets/portfolio2.png";
 import koovs from "../../Assets/portfolio3.png";
 import { SiRedux, SiChakraui } from "react-icons/si";
 import { FaCss3Alt, FaHtml5, FaReact } from "react-icons/fa";
-// import { MdMilitaryTech } from "react-icons/md";
+
 import { DiMongodb } from "react-icons/di";
 
 const Allproject = [
@@ -27,7 +27,6 @@ const Allproject = [
       "Uboric is an Ecommerce that delivers throughout India. You can shop for the desired product for your home and get them delivered to your doorstep.",
     git: "https://github.com/Mohit-Raj-Singh/-daily-purpose-3625",
     link: "https://u-shopic.vercel.app/",
-    // tech: "React.js | React-Redux , JavaScript | Chakra-UI",
     tech: [
       <FaCss3Alt size={"44px"} />,
       <FaReact size={"44px"} />,
@@ -42,7 +41,6 @@ const Allproject = [
       "Booking.com is an online travel agency for lodging reservations.",
     git: "https://github.com/Mohit-Raj-Singh/Booking.com-Clone",
     link: "https://booking-come-1562.netlify.app/",
-    // tech: "React.js | React-Redux , JavaScript | Chakra-UI",
     tech: [
       <FaCss3Alt size={"44px"} />,
       <FaReact size={"44px"} />,
@@ -57,7 +55,6 @@ const Allproject = [
       "Clone of Koovs.com, koovs is a Ecomerse related website which helps you to find the different kinds of the products.",
     git: "https://github.com/Mohit-Raj-Singh/Koovs-Clone",
     link: "https://frontend-mohit-6164.vercel.app/",
-    // tech: "React.js | React-Redux , JavaScript | Chakra-UI | Node.js | Express.js | MongoDB",
     tech: [
       <FaCss3Alt size={"44px"} />,
       <FaReact size={"44px"} />,
@@ -72,12 +69,10 @@ const Allproject = [
       "India's leading rental platform brings to you a wide range of products on rent. Experience the freedom to live the life you want, now.",
     git: "https://github.com/pratiksontakke/prompt-advertisement-8166",
     link: "https://jovial-dusk-c6ab4b.netlify.app/",
-    // tech: "HTML | CSS | Javascript ",
     tech: [
       <FaHtml5 size={"44px"} />,
       <FaCss3Alt size={"44px"} />,
       <FaReact size={"44px"} />,
-      // <SiChakraui size={"44px"} />,
     ],
   },
 ];
@@ -110,7 +105,6 @@ function Project() {
           <VStack
             boxShadow={"rgba(0, 0, 0, 0.24) 0px 3px 8px"}
             key={title}
-            // w={"500px"}
             m={"50px"}
             w={{ base: "250px", sm: "300px", md: "300px", lg: "400px" }}
             aline={"center"}
@@ -125,7 +119,6 @@ function Project() {
               src={img}
               alt={title}
               w={{ base: "250px", sm: "300px", md: "300px", lg: "400px" }}
-              // h={{ base: "180px", sm: "180px", md: "220px", lg:"280px" }}
             />
             <Text color={"#f9f1ac "} fontSize="3xl" mt="100px">
               {title}
@@ -138,9 +131,6 @@ function Project() {
             >
               {description}
             </Text>
-            {/* <Text frontWidth={"lg"} color={"white"}>
-              Tech stack: {tech}
-            </Text> */}
 
             <HStack color={"#f9f1ac"}>
               <Box>{tech[0]}</Box>
